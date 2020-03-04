92879a072f4c0b2171514055f46598ad2b0c55a0
Jim Ecksi <jimecksi@gmail.com>
2020-03-03 22:38:44 -0700

    Create menuOption component

---
+++ src/components/atoms/MenuOption.vue
@@ -0,0 +1,36 @@
+<template>
+	<section>
+		<img :src="require(`../../assets/menus/${asset}.svg`)" alt="Start icon" width="50">
+		<h3><slot></slot></h3>
+	</section>
+</template>
+
+<script>
+	export default {
+		name: 'MenuOption',
+		props: {
+			asset: {
+				type: String,
+				default: 'finish',
+				required: true
+			}
+		}
+	}
+</script>
+
+<style scoped>
+  section {
+    display: flex;
+    padding: 1.5rem;
+  }
+
+	section:hover {
+		background-color: purple;
+	}
+
+	section h3 {
+    align-self: center;
+    font-size: 28px;
+    margin-left: 3rem;
+  }
+</style>
