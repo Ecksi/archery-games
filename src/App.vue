495e32248bd52dbd62b0e132b6eae00a0c4a5d79
Jim Ecksi <jimecksi@gmail.com>
2020-03-03 20:49:32 -0700

    Add html skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <!-- //TODO: header component -->
+    <header class="header">
+      <img class="header__logo" src="./assets/logo.png" alt="Archery Games logo" width="75">
+      <div class="header__title">
+        <h2>Denver <span>League</span></h2>
+        <img src="./assets/menus/target.svg" alt="A target with an arrow in the center" width="50">
+      </div>
+    </header>
+    <main class="main">
+      <!-- //TODO: component list items -->
+      <section class="section">
+        <img src="./assets/menus/start.svg" alt="Start icon" width="50">
+        <h3>Start a match</h3>
+      </section>
+      <section class="section section-b">
+        <img src="./assets/menus/schedule.svg" alt="Schedule icon" width="50">
+        <h3>Schedule</h3>
+      </section>
+    </main>
   </div>
 </template>
 
