51b7c9ddb515f73507810bdefa888752d0ae14bc
Jim Ecksi <jimecksi@gmail.com>
2020-03-03 22:39:41 -0700

    Create userMenu

---
+++ src/components/molecules/UserMenu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <section v-for="(menu, index) in menus" :key="index">
+      <menu-option :asset="menu.img">{{ menu.text }}</menu-option>
+    </section>
+  </div>
+</template>
+
+<script>
+  import MenuOption from '../atoms/MenuOption';
+  
+  export default {
+    name: 'UserMenu',
+    components: {
+      MenuOption
+    },
+    data() {
+      return {
+        menus: [
+          {'img': 'schedule', 'text': 'Schedule'},
+          {'img': 'teams', 'text': 'Teams'},
+          {'img': 'players', 'text': 'Players'},
+          {'img': 'past-seasons', 'text': 'Past Seasons'},
+          {'img': 'contact', 'text': 'Contact'}
+        ]
+      }
+    }
+  }
+</script>
