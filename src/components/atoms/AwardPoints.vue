142b6af305816eb20a78ee7e60041a018e19a914
Jim Dechant <james.dechant@homeadvisor.com>
2020-03-06 13:26:10 -0700

    Add AwardPoints component

---
+++ src/components/atoms/AwardPoints.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>award winner</h1>
+    <img class="winner" src="../../assets/img/winner.svg" alt="A gold medal" height="50" width="50">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AwardPoints'
+  }
+</script>
+
+<style scoped>
+  .winner {
+    margin-top: 24px;
+  }
+</style>
