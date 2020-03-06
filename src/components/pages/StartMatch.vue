a09db13aaa6fbbd27cae171521443e0b94f0843c
Jim Ecksi <jimecksi@gmail.com>
2020-03-05 23:22:07 -0700

    Add start structure

--- src/components/pages/StartMatch.vue
+++ src/components/pages/StartMatch.vue
@@ -1,5 +1,33 @@
 <template>
-  <h1>welcome to the start page</h1>
+  <div>
+    <section class="team-scores">
+      <div>
+        <h1>Team A Name</h1>
+        <span>X</span>
+      </div>
+      <div>
+        <h1>Team A Name</h1>
+        <span>X</span>
+      </div>
+    </section>
+    <section class="award-points">
+      <aside>
+        <h1>award winner</h1>
+        <img src="" alt="">
+      </aside>
+      <aside>
+        <h1>award winner</h1>
+        <img src="" alt="">
+      </aside>
+    </section>
+    <section class="current-round">
+      <h3>- Round X -</h3>
+      <h1>Jailbreak</h1>
+    </section>
+    <section class="start-button">
+      <button>Start</button>
+    </section>
+  </div>
 </template>
 
 <script>
