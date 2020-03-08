bc30690fa3f4b2e6b158013c735c9414b0bdc7cd
ecksi <jimecksi@gmail.com>
2020-03-08 14:58:09 -0600

    Setup matchTimer

---
+++ src/components/atoms/MatchTimer.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="start-button" @click="toggleTimer">
+    <span v-if="!$store.state.match.isStarted && !$store.state.match.status">Start</span>
+    <span v-else>{{ $store.state.match.status === 'active' ? `Pause - ${$store.state.match.duration}` : `Resume - ${$store.state.match.duration}` }}</span>
+  </button>
+</template>
+
+<script>
+  import {mapActions} from 'vuex';
+
+  export default {
+    name: 'MatchTimer',
+    methods: {
+      ...mapActions(['toggleTimer'])
+    }
+  }
+</script>
+
+<style>
+  .start-button {
+    width: 100%;
+    height: 70px;
+    background-color: #009B4C;
+    border: none;
+    outline: none;
+    font-size: 24px;
+    font-weight: bold;
+    color: #FFF;
+    letter-spacing: 2px;
+    cursor: pointer;
+  }
+
+  .start-button span {
+    display: inline-block;
+    width: 100%;
+    filter: drop-shadow(0 2px 1px rgb(58, 58, 58));
+  }
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -18,7 +18,9 @@ export default new Vuex.Store({
       homeScore: 0,
       awayTeam: 'Test team for long names',
       awayScore: 3,
-      matchDuration: '4:00',
+      duration: '1:02',
+      isStarted: false,
+      status: '',
       round: 1,
       gameList: ['Elimination', 'Jail Break', 'Headcount', 'Elimination - 2 Barr - Dodgeball', 'Jail Break', 'Elimination - 2 Barr', 'Headcount', 'Team Choice', 'Y.O.L.O.', 'Dog Eat Dog']
     }
@@ -40,9 +42,50 @@ export default new Vuex.Store({
     },
     changeGame(state) {
       state.match.gameList[state.match.round -1]
+    },
+    toggleMatch(state) {
+      if (!state.match.isStarted) {
+        state.match.isStarted = true;
+        state.match.status = 'active'
+      } else {
+        state.match.isStarted = false;
+        state.match.status = 'paused'
+      }      
+    },
+    runTimer(state) {
+      let minutes = state.match.duration.split(':')[0];
+      let seconds = state.match.duration.split(':')[1];
+
+      if (minutes === '0' && seconds === '00') {
+        return;
+      }
+
+      if (seconds === '00') {        
+        seconds = '59'
+        minutes > 0 ? minutes-- : minutes
+      } else {
+        if (seconds < 11) {
+          seconds--
+          seconds = `0${seconds}`
+        } else {
+          seconds--
+        }
+      }
+
+      state.match.duration = `${minutes}:${seconds}`;
     }
   },
   actions: {
+    // Something buggy is going on with this timer when you pause/resume
+    toggleTimer({ commit, state }) {
+      commit('toggleMatch');
+  
+      setInterval(() => {
+        !state.match.isStarted || state.match.duration === '0:00'
+          ? clearInterval()
+          : commit('runTimer');
+      }, 1000)
+    }
   },
   modules: {
   }
