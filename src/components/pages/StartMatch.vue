0f4bd5ac7986974e01aa0fcbb722466c25f0c88f
ecksi <jimecksi@gmail.com>
2020-03-08 13:01:06 -0600

    Clean up StartMatch route

--- src/components/pages/StartMatch.vue
+++ src/components/pages/StartMatch.vue
@@ -1,40 +1,25 @@
 <template>
   <div class="start">
-    <section class="team-scores split h-full">
-      <!-- limit chars to 24 -->
-      <team-score>Knocked Out</team-score>
-      <team-score>Test for long team namesa</team-score>
-    </section>
-    <section class="award-points h-full">
-      <award-points></award-points>
-    </section>
-    <section class="match-info center h-full">
-        <!-- Call store for round data  -->
-      <h3 class="round">- Round X -</h3>
-      <div class="current-round">
-        <paginate>Jailbreak</paginate>
-      </div>
-    </section>
-    <section>
-      <!-- Add timer functionality -->
-      <button class="start-button">
-        <span>Start</span>
-      </button>
-    </section>
+    <team-score class="flex-one" />
+    <award-points class="flex-one" />
+    <game-list class="flex-one translate-y" />
+    <match-timer />
   </div>
 </template>
 
 <script>
   import TeamScore from '@/components/atoms/TeamScore';
   import AwardPoints from  '@/components/atoms/AwardPoints';
-  import Paginate from '@/components/atoms/Paginate';
+  import GameList from '@/components/atoms/GameList';
+  import MatchTimer from '@/components/atoms/MatchTimer'
 
   export default {
     name: 'StartMatch',
     components: { 
       TeamScore,
       AwardPoints,
-      Paginate
+      GameList,
+      MatchTimer
     }
   }
 </script>
@@ -47,56 +32,12 @@
     height: calc(100vh - 129px);
   }
 
-  .h-full {
+  .flex-one {
     flex: 1;
   }
 
-  .split {
-    display: flex;
-    justify-content: space-between;
-    height: 150px;
-    text-align: center;
-  }
-
-  .split div {
-    width: 50%;
-  }
-
-  .center {
-    text-align: center;
-  }
-
-  .round {
-    margin-top: 24px;
-    margin-bottom: 16px;
-  }
-
-  .current-round {
-    display: flex;
-    justify-content: space-around;
-  }
-
-  .match-info span {
-    font-size: 32px;
-  }
-
-  .match-info h1 {
-    padding-top: 8px;
-  }
-  
-  .start-button {
-    width: 100%;
-    height: 70px;
-    background-color: #009B4C;
-    border: none;
-    outline: none;
-    font-size: 24px;
-    font-weight: bold;
-    color: #FFF;
-    letter-spacing: 2px;
-  }
-
-  .start-button span {
-    filter: drop-shadow(0 2px 1px rgb(58, 58, 58));
+  .translate-y {
+    /* Disable this for iPhone 5/SE */
+    transform: translateY(35%);
   }
 </style>
