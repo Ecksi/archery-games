adf979d95730aa22859c2f773412bd9a0f939f70
Jim Ecksi <jimecksi@gmail.com>
2020-03-10 18:05:45 -0600

    Create MatchInfo component and update Schedule Page

---
+++ src/components/atoms/MatchInfo.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <h2 class="time">{{ startTime }}</h2>
+    <section class="match bg-color">
+      <h2 class="team-a">{{ homeTeam }}</h2>
+      <img src="../../assets/img/versus.png" alt="" height=50 width=50>
+      <h2 class="team-a">{{ awayTeam }}</h2>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MatchInfo',
+    props: {
+      startTime: {
+        type: String,
+        default: '2:30'
+      },
+      homeTeam: {
+        type: String,
+        default: 'Home Team'
+      },
+      awayTeam :{
+        type: String,
+        default: 'Away Team'
+      }
+    },
+    mounted: {
+      test() {
+        console.log('sup', this.startTime)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .time {
+    font-size: 32px;
+  }
+
+  .match {
+    display: flex;
+    justify-content: center;
+    margin: 16px 0;
+  }
+
+  /* pull team color, create gradient off that */
+  .bg-color {
+    background-color: rgba(135, 206, 250, 0.5);
+  }
+
+  section h2 {
+    width: 125px;
+    font-size: 28px;
+    line-height: 1;
+  }
+
+  section img {
+    margin: auto 16px;
+  }
+
+  .team-a, .team-b {
+    border: 1px solid red;
+    height: 100%;
+    padding: 24px;
+    cursor: pointer;
+  }
+</style>
--- src/components/pages/Schedule.vue
+++ src/components/pages/Schedule.vue
@@ -1,39 +1,27 @@
 <template>
   <div class="schedule">
-    <!-- <paginate>March 20</paginate> -->
-    <!-- component times --> 
     <div>
-      <h2 class="time">6:30</h2>
-      <section class="match">
-        <h2>team a name</h2>
-        <img src="../../assets/img/versus.png" alt="" height=50 width=50>
-        <h2>team b name</h2>
-      </section>
+      <h2>March 20</h2>
+      <h3>Week 7</h3>
     </div>
-
-    <div>
-      <h2 class="time">7:30</h2>
-      <section class="match">
-        <h2>team a name</h2>
-        <img src="../../assets/img/versus.png" alt="" height=50 width=50>
-        <h2>team b name</h2>
-      </section>
-    </div>
-
-    <div>
-      <h2 class="time">8:30</h2>
-      <section class="match">
-        <h2>team a name</h2>
-        <img src="../../assets/img/versus.png" alt="" height=50 width=50>
-        <h2>team b name</h2>
-      </section>
+    <hr />
+    <div v-for="(time, index) in startTimes" :key="index">
+      <match-info :startTime="time" />
     </div>
   </div>
 </template>
 
 <script>
+  import MatchInfo from '@/components/atoms/MatchInfo';
+  
   export default {
-    name: 'Schedule'
+    name: 'Schedule',
+    components: {
+      MatchInfo
+    },
+    data: () => ({
+      startTimes: ['6:30', '7:30', '8:30']
+    })
   }
 </script>
 
@@ -42,42 +30,11 @@
     margin: 16px;
   }
 
-  header {
-    display: flex;
-    justify-content: space-around;
-    margin-bottom: 24px;
-  }
-
-  header h1 {
-    padding-top: 4px;
-  }
-
-  header span {
-    font-size: 32px;
-  }
-
-  h2 {
+  h2, h3 {
     text-align: center;
   }
 
-  .time {
-    margin-top: 28px;
-    margin-bottom: 24px;
-    font-size: 32px;
-  }
-
-  section h2 {
-    width: 125px;
-    font-size: 28px;
-    line-height: 1;
-  }
-
-  section img {
-    margin: 0 16px;
-  }
-
-  .match {
-    display: flex;
-    justify-content: center;
+  hr {
+    margin: 12px;
   }
 </style>
