daec921530febecc2b5d2ef62a525225c75abcdb
Jim Ecksi <jimecksi@gmail.com>
2020-03-25 19:51:42 -0600

    Add tailwind to app

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+import '@/assets/css/tailwind.css';
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
