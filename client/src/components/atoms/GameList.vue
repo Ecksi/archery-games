1d3b02253c620778350838171b2f20d3603b9ee4
ecksi <jimecksi@gmail.com>
2020-03-25 20:19:48 -0600

    Convert GameList styles into tailwind

--- client/src/components/atoms/GameList.vue
+++ client/src/components/atoms/GameList.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <h3 class="round" v-if="$store.state.match.round === 10">- Bonus Round -</h3>
-    <h3 class="round" v-else>- Round {{ $store.state.match.round }} -</h3>
-    <div class="game-list">
-      <span class="arrow" @click="previousGame">&lt;</span>
-        <h2 class="current-game">{{ $store.state.match.gameList[$store.state.match.round - 1] }}</h2>
-      <span class="arrow" @click="nextGame">&gt;</span>
+    <h3 class="mb-4 text-center" v-if="$store.state.match.round === 10">- Bonus Round -</h3>
+    <h3 class="mb-4 text-center" v-else>- Round {{ $store.state.match.round }} -</h3>
+    <div class="flex justify-around my-0 mx-4">
+      <span class="cursor-pointer text-4xl" @click="previousGame">&lt;</span>
+        <h2 class="current-game pt-2 text-center">{{ $store.state.match.gameList[$store.state.match.round - 1] }}</h2>
+      <span class="cursor-pointer text-4xl" @click="nextGame">&gt;</span>
     </div>
   </div>
 </template>
@@ -22,25 +22,7 @@
 </script>
 
 <style scoped>
-  .round {
-    margin-bottom: 16px;
-    text-align: center;
-  }
-
-  .game-list {
-    display: flex;
-    margin: 0 16px;
-    justify-content: space-around;
-  }
-
-  .arrow {
-    font-size: 32px;
-    cursor: pointer;
-  }
-
   .current-game {
-    padding-top: 8px;
     width: 80vw;
-    text-align: center;
   }
 </style>
