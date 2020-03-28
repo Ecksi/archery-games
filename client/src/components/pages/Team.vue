bf3669fe14e07e02b44661c91217f05ede2659a1
Jim Ecksi <jimecksi@gmail.com>
2020-03-27 23:40:31 -0600

    Convert Team styles into tailwind

--- client/src/components/pages/Team.vue
+++ client/src/components/pages/Team.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="team">
+  <div class="my-0 mx-4">
     <h1>{{ team.name }}</h1>
     <h3>Color: {{ team.color }}</h3>
 
     Players:
     <section v-for="(member, index) in team.members" :key="index">
       <span>{{ member.name }}</span>
-      <img v-if="member.captain" src="../../assets/img/captain.png" alt="" height="15" class="captain">
+      <img v-if="member.captain"  class="ml-3" src="../../assets/img/captain.png" alt="A captains badge" height="15">
     </section>
   </div>
 </template>
@@ -23,13 +23,3 @@
     }
   }
 </script>
-
-<style scoped>
-  .team {
-    margin: 0 16px;
-  }
-
-  .captain {
-    margin-left: 12px;
-  }
-</style>
